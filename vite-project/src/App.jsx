2f4be291bec606407e05d6f9402ad7ee0d642e94
Betelhem Tekle <betelhemtekle2021@gmail.com>
2025-04-05 17:26:09 +0300

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+ 
+
+  return (
+   
+      <div>
+        <h1>P2P Transaction</h1>
+      </div>
+      
+      
+  )
+}
+
+export default App
